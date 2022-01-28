3b4c22adff19ebd62184a6cadb5f218ddaf9a2fa
Pablo Roberto Gonçalves da Silva <pablorgds@gmail.com>
2022-01-27 22:24:45 -0300

    correções de apontamento do path

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static('./dist/search-marvel'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/search-marvel/'}),
+  res.sendFile('index.html', {root: 'dist/search-marvel/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
