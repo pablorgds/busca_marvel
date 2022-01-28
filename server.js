4507200643a2d22c6458a7c4a350d6d4d3c9e0b1
Pablo Roberto Gonçalves da Silva <pablorgds@gmail.com>
2022-01-27 23:40:28 -0300

    Teste de deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/search-marvel/src/'));
+app.use(express.static('./src/'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/search-marvel/src/'}),
+  res.sendFile('index.html', {root: 'src/'}),
 );
 
 // Start the app by listening on the default Heroku port
