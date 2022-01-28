4df40a9472f329083b3b495b7164db55f950e295
Pablo Roberto Gonçalves da Silva <pablorgds@gmail.com>
2022-01-28 00:27:12 -0300

    Teste de deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/'));
+app.use(express.static(path.join(__dirname,'./src/')));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'src/'}),
+  res.sendFile(path.join(__dirname,'./src/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
